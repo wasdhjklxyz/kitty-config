1c312f4164d8722d0056a9f1377f6045261dfe02
uiop <uiop@wasdhjkl.xyz>
2025-10-02 17:59:05 -0400

    feat(colors): add gnome auto themeing

---
+++ scripts/auto-theme-gnome.sh
@@ -0,0 +1,9 @@
+if [ -n "$KITTY_PID" ]; then
+  GNOME_COLOR_SCHEME=$(gsettings get org.gnome.desktop.interface color-scheme)
+  if [[ "${GNOME_COLOR_SCHEME//\'/}" == "default" ]]; then
+    export KITTY_COLORS="light"
+  else
+    export KITTY_COLORS="dark"
+  fi
+  kitty @ set-colors --all "~/.config/kitty/colors/$KITTY_COLORS.conf"
+fi
